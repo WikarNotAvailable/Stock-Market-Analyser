2768991631b5e8e77772ad619c79bf09bd0f5c7c
WikarNotAvailable <wikar8998@gmail.com>
2023-11-02 19:57:22 +0100

    finished endpoints for usertype

--- StockMarketAnalyserAPI/src/controllers/usertypes_controller.py
+++ StockMarketAnalyserAPI/src/controllers/usertypes_controller.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, request
 from src.constants.__http_status_codes import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_200_OK
 from src.models.usertype import Usertype
-from sqlalchemy import insert, exc, select
-from src.models.base import Base
+from sqlalchemy import delete, exc, select, update
+from sqlalchemy.orm import Session
 from flask.json import jsonify
-import validators
 
 
 def construct_usertypes_controller(engine):
@@ -14,38 +13,101 @@ def construct_usertypes_controller(engine):
     def post_usertype():
         usertype_name = request.get_json().get('Usertype', '')
 
-        stmt = (
-            insert(Base.metadata.tables[Usertype.__tablename__]).values(Usertype=usertype_name)
-        )
-        with engine.connect() as conn:
-            try:
-                result = conn.execute(stmt)
-            except exc.IntegrityError as error:
-                if "UniqueViolation" in str(error):
-                    return jsonify({
-                        'error': 'Usertype has been already added to database'
-                    }), HTTP_400_BAD_REQUEST
-                else:
-                    raise Exception(str(error))
-            conn.commit()
+        usertype = Usertype(Usertype=usertype_name)
+
+        with Session(engine) as session:
+            with session.begin():
+                try:
+                    session.add(usertype)
+                    session.commit()
+                except exc.IntegrityError as error:
+                    session.rollback()
+                    if "UniqueViolation" in str(error):
+                        return jsonify({
+                            'error': 'Usertype already exists in database'
+                        }), HTTP_400_BAD_REQUEST
+                    else:
+                        raise Exception(str(error))
+            session.refresh(usertype)
 
         return jsonify({
-            'Usertype': usertype_name
+            'UsertypeID': usertype.UsertypeID, 'Usertype': usertype.Usertype
         }), HTTP_201_CREATED
 
     @usertypes_controller.get('/')
     def get_all_usertypes():
-        stmt = (
-            select(Base.metadata.tables[Usertype.__tablename__])
-        )
-        with engine.connect() as conn:
-            result = conn.execute(stmt)
+        stmt = select(Usertype)
+
+        with Session(engine) as session:
+            usertypes = session.execute(stmt).scalars().all()
+
+            data = []
+            for usertype in usertypes:
+                data.append({'UsertypeID': usertype.UsertypeID, 'Usertype': usertype.Usertype})
 
             return jsonify({
-                'Usertypes': [row[0] for row in result.fetchall()]
+                'Usertypes': {'data': data}
             }), HTTP_200_OK
 
+    @usertypes_controller.get('/<int:id>')
+    def get_usertype(id):
+        stmt = select(Usertype).where(Usertype.UsertypeID == id)
+
+        with Session(engine) as session:
+            usertype = session.execute(stmt).scalar()
+            if usertype is None:
+                return jsonify({
+                     'error': 'Item with passed id was not found in database'
+                }), HTTP_400_BAD_REQUEST
+
+            return jsonify({
+                'UsertypeID': usertype.UsertypeID, 'Usertype': usertype.Usertype
+            }), HTTP_200_OK
+
+    @usertypes_controller.delete('/<int:id>')
+    def delete_usertype(id):
+        item = get_usertype(id)
+        if item[1] is HTTP_400_BAD_REQUEST:
+            return item
+
+        stmt = delete(Usertype).where(Usertype.UsertypeID == id)
+
+        with Session(engine) as session:
+            session.execute(stmt)
+
+        return jsonify({
+            'response': "deleted"
+        }), HTTP_200_OK
+
+    @usertypes_controller.put('/<int:id>')
+    def update_usertype(id):
+        item = get_usertype(id)
+        if item[1] is HTTP_400_BAD_REQUEST:
+            return item
+
+        usertype_name = request.get_json().get('Usertype', '')
+
+        stmt = update(Usertype).where(Usertype.UsertypeID == id).values(Usertype=usertype_name)
+
+        with Session(engine) as session:
+            try:
+                session.execute(stmt)
+            except exc.IntegrityError as error:
+                session.rollback()
+                if "UniqueViolation" in str(error):
+                    return jsonify({
+                        'error': 'Usertype already exists in database'
+                    }), HTTP_400_BAD_REQUEST
+                else:
+                    raise Exception(str(error))
+
+        return jsonify({
+            'UsertypeID': id, 'Usertype': usertype_name
+        }), HTTP_200_OK
+
     return usertypes_controller
+
+
 
 
 
